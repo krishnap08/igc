de321dc590a8a82c14d6f66e328e67806b6cd395
“Upendar <dupendar44@gmail.com>
2018-06-29 20:43:09 +0530

    initial commit

---
+++ src/main/java/com/bnsf/igcserviceapp/IgcServiceAppApplication.java
@@ -0,0 +1,12 @@
+package com.bnsf.igcserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgcServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgcServiceAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bnsf/igcserviceapp/IgcServiceAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bnsf.igcserviceapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IgcServiceAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
